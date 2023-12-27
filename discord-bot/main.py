3d48b5a4a9a2701db3934c0b178ec3efb02f3766
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2023-12-27 17:14:38 -0500

    add initial slash commands

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -1,7 +1,13 @@
 import os
+import requests
 import discord
 from discord import app_commands
 
+'''
+--------------
+SETUP
+--------------
+'''
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,13 +15,80 @@ intents.members = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+BACKEND_API = "https://backend-production-c33b.up.railway.app"
+
+'''
+--------------
+SLASH COMMANDS
+--------------
+'''
+
 @tree.command(
-    name="commandname",
-    description="My first application Command",
+    name="info",
+    description="Get info about the bot",
     guild=discord.Object(id=1187847033596432394)
 )
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
+async def info_command(interaction):
+    response = "Hello! /n\
+I am a bot created by @Goldeneyes, /n\
+I'm here to help you curate the SpellTable Pals experience. /n\
+*For information about my commands, type /help.*"        
+    await interaction.response.send_message(response, ephemeral=True)
+    
+@tree.command(
+    name="help",
+    description="Get help with the bot",
+    guild=discord.Object(id=1187847033596432394)
+)
+async def help_command(interaction):
+    response = "Here are my commands: /n\
+*/info* - Get info about the bot /n\
+*/help* - Get help with the bot /n\
+*/ping* - Get the bot's latency /n\
+*/block* - Submits a block request for a given SpellTable user. /n\
+If you have any questions, please contact @Goldeneyes."       
+    await interaction.response.send_message(response, ephemeral=True)
+    
+@tree.command(
+    name="ping",
+    description="Get the bot's latency",
+    guild=discord.Object(id=1187847033596432394)
+)
+async def ping_command(interaction):
+    response = f"Pong! {round(client.latency * 1000)}ms"        
+    await interaction.response.send_message(response, ephemeral=True)
+    
+@tree.command(
+    name="block",
+    description="Submits a block request for a given SpellTable user",
+    guild=discord.Object(id=1187847033596432394)
+)
+async def block_command(interaction, username, reason):
+    if username == None or reason == None:
+        response = "Please provide a username and a reason."
+        await interaction.response.send_message(response, ephemeral=True)
+        return
+    
+    api_response = requests.post(f"{BACKEND_API}/block_user", json={"username": username, "reason": reason})
+    
+    if api_response.status_code != 200:
+        response = "Something went wrong. Please try again later."
+        await interaction.response.send_message(response, ephemeral=True)
+        return
+    
+    if api_response.json()["status"] != "Success":
+        if api_response.json()["status"] == "Failed: chill":
+            response = "User is Certified Chill, please contact a moderator if you would like to proceed."
+            await interaction.response.send_message(response, ephemeral=True)
+            return
+        
+        response = "Something went wrong. Please try again later."
+        await interaction.response.send_message(response, ephemeral=True)
+        return
+        
+    response = f"Block request submitted for {username} with reason: {reason}."        
+    await interaction.response.send_message(response, ephemeral=True)
+    return
     
 @client.event
 async def on_ready():
