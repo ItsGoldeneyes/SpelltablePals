abbbb4dadf0bde8f87e76fe957effca290141ed7
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2024-01-11 02:01:20 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -231,7 +231,10 @@ def update_pals(user_profiles):
             # If the user's username exists in the database, but does not have a discord_id associated with it, assume this is them
             potential_user = Spelltableusers.query.filter(Spelltableusers.username == user_profiles[user]['username']).first()
             
-            if potential_user.discord_id == None:
+            if potential_user == None:
+                continue
+            
+            elif potential_user.discord_id == None:
                 print(f"User {user_profiles[user]['username']}'s username found in database. Adding discord_id.")
                 potential_user.discord_id = user
                 potential_user.changed_on = datetime.datetime.now()
