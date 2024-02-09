04256fbddf610290b7471fc7c6b579de4ab33635
Tyler Mueller <muelltyl@gmail.com>
2024-02-09 16:58:17 -0600

    adds build pipeline with scripts

---
+++ extension/build.ps1
@@ -0,0 +1,5 @@
+# Copy all files except those with a .ts extension from src to dist
+Get-ChildItem -Path "./src/" -File | Where-Object { $_.Extension -ne ".ts" -and $_.Name -ne "tsconfig.json" } | ForEach-Object { Copy-Item $_.FullName -Destination "./dist/" }
+
+# Compile TypeScript files from src to JavaScript, outputting to dist
+tsc -p ./src --outDir ./dist;
