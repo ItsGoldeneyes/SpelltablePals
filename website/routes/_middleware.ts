5b395fb8219403e41883d8649dc8828a88761220
Tyler Mueller <muelltyl@gmail.com>
2024-02-21 16:13:50 -0600

    moves middlware to different files

--- website/routes/_middleware.ts
+++ website/routes/_middleware.ts
@@ -1,24 +1,5 @@
-import { FreshContext } from "$fresh/server.ts";
+import { DiscordLinkMiddleware } from "../middleware/mod.ts";
 
-const invite = "https://discord.com/invite/9QwzXz5Rt5";
-const disabled = false;
-
-// TODO(ybabts): Integrate these controls into the database or Discord bot and move this to a separate file
-
-export function handler(
-  req: Request,
-  ctx: FreshContext,
-) {
-  const url = new URL(req.url);
-  const path = url.pathname;
-  if (path === "/discord") {
-    if (disabled) {
-      return new Response(
-        "Our Discord is currently not taking any more invites, sorry",
-        { status: 403 },
-      );
-    }
-    return Response.redirect(invite, 307);
-  }
-  return ctx.next();
-}
+export const handler = [
+  DiscordLinkMiddleware,
+];
